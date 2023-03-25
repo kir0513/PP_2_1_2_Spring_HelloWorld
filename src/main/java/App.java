283a1245a90b9d71fb6e25e5beb8fa94095eaa8f
kir0513 <kir0513@yandex.ru>
2023-03-25 20:25:30 +0300

    Класс App содержит метод main, который запускает Спринг при создании контекста. В контекст мы передаем класс AppConfig, который помечен аннотацией @Configuration - так Спринг понимает, что это настроечный класс. В классе AppConfig содержится метод, помеченный аннотацией как бин, этот метод будет выполнен при запуске и его результат станет объектом, управляемым Спрингом. Класс HelloWorld является обычным Java-классом, мы его используем без каких-либо сложностей. В методе main мы можем получить бин HelloWorld по имени и использовать в любом месте программы. Спринг позволяет использовать различные типы бинов, одни будут жить от старта до завершения программы, другие будут создаваться при каждом запросе или открытии новой сессии, третьи при каждом вызове будут создаваться новые. За это отвечает аннотация @Scope.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class App {
+public class App {//
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
