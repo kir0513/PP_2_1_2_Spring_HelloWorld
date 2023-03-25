44706f3a4a49ec75fe9a8f042d35421ed8194c89
kir0513 <kir0513@yandex.ru>
2023-03-25 20:13:24 +0300

    Создан бин Cat

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+@Configuration//Указывает, что класс объявляет один или несколько @Bean методов и может обрабатываться контейнером
+// Spring для генерации определений компонентов и запросов на обслуживание для этих компонентов во время выполнения
 public class AppConfig {
  
-    @Bean(name="helloworld")
+    @Bean(name="helloworld")// используется для обьявления бина
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat(){
+        return new Cat();
     }
 }
